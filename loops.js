f3c149b13eda97dfb719e2255d3752626026a19f
Gabriela Fijalkowska <gabrielafijalkowska@gmail.com>
2017-11-05 17:05:39 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,10 +1,3 @@
-function whileLoop(countdown) {
-  while (countdown > 0) {
-    console.log(--countdown)
-  }
-  return 'done'
-}
-
 function forLoop(array) {
   for (let i = 0, l = 25; i < l; i++) {
     let s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
@@ -13,17 +6,24 @@ function forLoop(array) {
   return array
 }
 
+function whileLoop(countdown) {
+  while (countdown > 0) {
+    console.log(--countdown)
+  }
+  return 'done'
+}
+
 function maybeTrue() {
   return Math.random() >= 0.5
 }
 
 function doWhileLoop(array) {
-  let l = array.length - 1
+  let l = array.length
 
   do {
     array.splice(l, 1)
     l = l - 1
-  } while (l > -1 && maybeTrue())
+  } while (l > -1 || maybeTrue())
 
   return array
 }
